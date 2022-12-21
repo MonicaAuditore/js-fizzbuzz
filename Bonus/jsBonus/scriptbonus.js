f2408a9df2ce1e49004b9abd850cc2c2a6555579
MonicaAuditore <mauditore@live.it>
2022-12-21 17:58:07 +0100

    add_bonus 2 parte uno

--- Bonus/jsBonus/scriptbonus.js
+++ Bonus/jsBonus/scriptbonus.js
@@ -12,21 +12,25 @@ for (let i = 1; i < 101; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     const main = document.querySelector("main.flex");
     const div = document.createElement("div");
+    div.className = "fizzBuzz" + " box";
     div.append("FizzBuzz");
     main.append(div);
   } else if (i % 3 === 0) {
     const main = document.querySelector("main.flex");
     const div = document.createElement("div");
+    div.className = "fizz" + " box";
     div.append("fizz");
     main.append(div);
   } else if (i % 5 === 0) {
     const main = document.querySelector("main.flex");
     const div = document.createElement("div");
+    div.className = "buzz" + " box";
     div.append("buzz");
     main.append(div);
   } else {
     const main = document.querySelector("main.flex");
     const div = document.createElement("div");
+    div.className = "box";
     div.append(i);
     main.append(div);
   }
