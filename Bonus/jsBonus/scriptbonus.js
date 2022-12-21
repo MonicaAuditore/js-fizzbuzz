aeac93ebdea13cab99f765869fe1614f7d1fd11c
MonicaAuditore <mauditore@live.it>
2022-12-21 18:14:53 +0100

    add_BONUS 2 style

--- Bonus/jsBonus/scriptbonus.js
+++ Bonus/jsBonus/scriptbonus.js
@@ -19,13 +19,13 @@ for (let i = 1; i < 101; i++) {
     const main = document.querySelector("main.flex");
     const div = document.createElement("div");
     div.className = "fizz" + " box";
-    div.append("fizz");
+    div.append("Fizz");
     main.append(div);
   } else if (i % 5 === 0) {
     const main = document.querySelector("main.flex");
     const div = document.createElement("div");
     div.className = "buzz" + " box";
-    div.append("buzz");
+    div.append("Buzz");
     main.append(div);
   } else {
     const main = document.querySelector("main.flex");
