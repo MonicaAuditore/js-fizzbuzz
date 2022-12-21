ca1ef805045e9f5da2e12f215150d0cdc00d8909
MonicaAuditore <mauditore@live.it>
2022-12-21 16:13:12 +0100

    add_inizio bonus

--- Bonus/jsBonus/scriptbonus.js
+++ Bonus/jsBonus/scriptbonus.js
@@ -33,27 +33,27 @@
 //   contenitore.append(d);
 // }
 
-// for (let i = 1; i < 101; i++) {
-//   if (i % 3 === 0) {
-//     i = "Fizz";
-//   } else {
-//     i = i;
-//   }
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0) {
+    i = "Fizz";
+  } else {
+    i = i;
+  }
 
-//   if (i % 5 === 0) {
-//     i = "Buzz";
-//   } else {
-//     i = i;
-//   }
+  if (i % 5 === 0) {
+    i = "Buzz";
+  } else {
+    i = i;
+  }
 
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     i = "FizzBuzz";
-//   } else {
-//     i = i;
-//   }
+  if (i % 3 === 0 && i % 5 === 0) {
+    i = "FizzBuzz";
+  } else {
+    i = i;
+  }
 
-//   const contenitore = document.querySelector(".container");
-//   let d = document.createElement(`div`);
-//   d.append(i);
-//   contenitore.append(d);
-// }
+  const contenitore = document.querySelector(".container");
+  let d = document.createElement(`div`);
+  d.append(i);
+  contenitore.append(d);
+}
