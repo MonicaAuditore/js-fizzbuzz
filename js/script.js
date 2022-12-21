fbfbf39a9fad6c9078e041c2d316ec84232ed4ba
MonicaAuditore <mauditore@live.it>
2022-12-21 15:59:44 +0100

    add_prova in html

--- js/script.js
+++ js/script.js
@@ -22,15 +22,71 @@ Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda ch
 4. se il numero è divisibile per 3 e divisibile per 5 al suo interno inserisci la stringa FizzBuzz e stampa in console;
 */
 
+// const contenitore = document.querySelector(".container");
+// let d = document.createElement(`div`);
+// d.append(i);
+// contenitore.append(d);
+
+// for (let i = 1; i < 101; i++) {
+//   console.log(i);
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i + "FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log(i + "Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log(i + "Buzz");
+//   }
+// }
+
+// for (let i = 1; i < 101; i++) {
+//   const contenitore = document.querySelector(".container");
+//   let d = document.createElement(`div`);
+//   console.log(d);
+//   d.append(i);
+//   contenitore.append(d);
+
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i + "FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log(i + "Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log(i + "Buzz");
+//   }
+// }
+
+// for (let i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     i = "FizzBuzz";
+//   } else if (i % 3 === 0) {
+//     i = "Fizz";
+//   } else if (i % 5 === 0) {
+//     i = "Buzz";
+//   }
+//   const contenitore = document.querySelector(".container");
+//   let d = document.createElement(`div`);
+//   d.append(i);
+//   contenitore.append(d);
+// }
+
 for (let i = 1; i < 101; i++) {
-  console.log(i);
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i + "FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log(i + "Fizz");
-  } else if (i % 5 === 0) {
-    console.log(i + "Buzz");
+  if (i % 3 === 0) {
+    i = "Fizz";
+  } else {
+    i = i;
   }
+
+  if (i % 5 === 0) {
+    i = "Buzz";
+  } else {
+    i = i;
+  }
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    i = "FizzBuzz";
+  } else {
+    i = i;
+  }
+
   const contenitore = document.querySelector(".container");
   let d = document.createElement(`div`);
   d.append(i);
