7080a5e67a300975ebec5368f83c477232a0b5a5
MonicaAuditore <mauditore@live.it>
2022-12-21 16:08:33 +0100

    add_esercizio completato parte uno

---
+++ Bonus/jsBonus/scriptbonus.js
@@ -0,0 +1,59 @@
+// const contenitore = document.querySelector(".container");
+// let d = document.createElement(`div`);
+// d.append(i);
+// contenitore.append(d);
+
+// for (let i = 1; i < 101; i++) {
+//   const contenitore = document.querySelector(".container");
+//   let d = document.createElement(`div`);
+//   console.log(d);
+//   d.append(i);
+//   contenitore.append(d);
+
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i + "FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log(i + "Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log(i + "Buzz");
+//   }
+// }
+
+// for (let i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     i = "FizzBuzz";
+//   } else if (i % 3 === 0) {
+//     i = "Fizz";
+//   } else if (i % 5 === 0) {
+//     i = "Buzz";
+//   }
+//   const contenitore = document.querySelector(".container");
+//   let d = document.createElement(`div`);
+//   d.append(i);
+//   contenitore.append(d);
+// }
+
+// for (let i = 1; i < 101; i++) {
+//   if (i % 3 === 0) {
+//     i = "Fizz";
+//   } else {
+//     i = i;
+//   }
+
+//   if (i % 5 === 0) {
+//     i = "Buzz";
+//   } else {
+//     i = i;
+//   }
+
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     i = "FizzBuzz";
+//   } else {
+//     i = i;
+//   }
+
+//   const contenitore = document.querySelector(".container");
+//   let d = document.createElement(`div`);
+//   d.append(i);
+//   contenitore.append(d);
+// }
