6696ed288bbf2233d9065a726769dbb694551c01
MonicaAuditore <mauditore@live.it>
2022-12-22 10:15:35 +0100

    fix_pulizia codice

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda ch
 
 for (let i = 1; i < 101; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i + "FizzBuzz");
+    console.log("FizzBuzz");
   } else if (i % 3 === 0) {
-    console.log(i + "Fizz");
+    console.log("Fizz");
   } else if (i % 5 === 0) {
-    console.log(i + "Buzz");
+    console.log("Buzz");
   } else {
     console.log(i);
   }
